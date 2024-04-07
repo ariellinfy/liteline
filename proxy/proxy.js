9f365448710bd86f8aad0ea2ee6fea37b86baefe
Ariel Lin <72408793+ariellinfy@users.noreply.github.com>
2024-04-07 11:12:42 -0600

    add health check

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
@@ -11,16 +12,45 @@ app.use(
   })
 );
 
-const servers = [
+const allServers = [
   "https://liteline.onrender.com",
   "https://liteline-api01.onrender.com",
   "https://liteline-api02-test.onrender.com",
 ];
 
+const healthyServers = new Set();
+
+const checkHealthyServers = async () => {
+  for (const server of allServers) {
+    try {
+      await axios.get(server + "/health");
+      // update health list
+      healthyServers.add(server);
+    } catch (error) {
+      console.log("Server unhealthy: ", server);
+    }
+  }
+};
+
 let currentServer = 0;
 
 const getServer = () => {
-  currentServer = (currentServer + 1) % servers.length;
+  if (healthyServers.size === 0) {
+    return "";
+  }
+
+  // check current server health
+  if (!healthyServers.has(allServers[currentServer])) {
+    // find next healthy server
+    for (let i = 0; i < allServers.length; i++) {
+      const nextServer = (currentServer + i) % allServers.length;
+      if (healthyServers.has(allServers[nextServer])) {
+        currentServer = nextServer;
+        break;
+      }
+    }
+  }
+
   return servers[currentServer];
 };
 
@@ -29,10 +59,29 @@ const proxyOptions = {
   changeOrigin: true,
   ws: true,
   secure: true,
+  on: {
+    proxyReq: (proxyReq, req, res) => {
+      /* handle proxyReq */
+      if (healthyServers.size === 0) {
+        res.status(500).send({
+          message: "No healthy servers",
+        });
+      }
+    },
+    proxyRes: (proxyRes, req, res) => {
+      /* handle proxyRes */
+    },
+    error: (err, req, res) => {
+      /* handle error */
+    },
+  },
 };
 
 const proxyMiddleware = createProxyMiddleware(proxyOptions);
 
 app.use(proxyMiddleware);
+
+// Update health periodically
+setInterval(checkHealthyServers, 5000);
 
 app.listen(5000);

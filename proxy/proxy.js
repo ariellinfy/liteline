d04a10d3171610a8a7957e110127261c5f32e5a9
Ariel Lin <72408793+ariellinfy@users.noreply.github.com>
2024-04-06 23:14:28 -0600

    proxy round-robin

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -3,8 +3,21 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
+const servers = [
+  "https://liteline.onrender.com",
+  "https://liteline-api01.onrender.com",
+  "https://liteline02.onrender.com",
+];
+
+let currentServer = 0;
+
+const getServer = () => {
+  currentServer = (currentServer + 1) % servers.length;
+  return servers[currentServer];
+};
+
 const proxyOptions = {
-  target: "https://liteline.onrender.com",
+  target: getServer(),
   changeOrigin: true,
   ws: true,
   secure: true,

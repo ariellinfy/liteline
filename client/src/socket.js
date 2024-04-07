e18946a8e4fd468035fd1be2008e20809633eb4d
Ariel Lin <72408793+ariellinfy@users.noreply.github.com>
2024-04-06 22:58:14 -0600

    update proxy url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
-const serverUrl = "https://liteline.onrender.com/";
-const socket = io(serverUrl, {
+// const serverUrl = "https://liteline.onrender.com/";
+const proxyUrl = "https://liteline-proxy-v48s.onrender.com/";
+const socket = io(proxyUrl, {
   autoConnect: false,
   withCredentials: true,
 });
--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -2,7 +2,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://liteline.onrender.com/", // server url
+    baseUrl: "https://liteline-proxy-v48s.onrender.com/", // server url
     credentials: "include",
   }),
   tagTypes: ["User", "Room", "Message"],

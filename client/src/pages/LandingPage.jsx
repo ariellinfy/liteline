8b8e1cc70b856727d95a0778540772f35f2ca00d
Ariel Lin <72408793+ariellinfy@users.noreply.github.com>
2024-04-10 12:37:52 -0600

    fixed router component error

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,22 +1,20 @@
 import { useSelector } from "react-redux";
-import { Redirect, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Authenticator from "../components/authenticator/Authenticator";
 
 const LandingPage = () => {
   const { userInfo } = useSelector((state) => state.user);
 
   return (
-    <Route
-      render={(props) =>
-        userInfo ? (
-          <div className="container-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500">
-            <Authenticator />
-          </div>
-        ) : (
-          <Redirect to="/chatroom" />
-        )
-      }
-    />
+    <>
+      {userInfo ? (
+        <div className="container-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500">
+          <Authenticator />
+        </div>
+      ) : (
+        <Navigate to="/chatroom" />
+      )}
+    </>
   );
 };
 
